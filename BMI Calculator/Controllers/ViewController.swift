2db86646cdec3e37d51e02124e446d018100d331
Maikel Ramos <mkramost@gmail.com>
2021-11-15 22:14:25 -0500

    label interaction, bmi calculation

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,10 +22,19 @@ class ViewController: UIViewController {
 
     @IBAction func heightSlider(_ sender: UISlider) {
         print(sender.value)
+        heightLabel.text = String(format: "%.2f", sender.value)+"m"
     }
     
     @IBAction func weightSlider(_ sender: UISlider) {
         print(Int(sender.value))
+        weightLabel.text = String(format: "%.0f", sender.value)+"Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight/pow(height,2)
+        print(bmi)
     }
 }
 
